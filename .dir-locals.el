0190a229e8e7d96fc4eacf57dd5d154e995df70a
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-01 09:26:58 +0800

    update: 添加代码逻辑

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ((c-mode
-  (indent-tabs-mode . nil)
-  (c-file-style . "bsd")
-  (c-basic-offset . 2)))
+   (indent-tabs-mode . nil)
+   (c-file-style . "bsd")
+   (c-basic-offset . 2)
+   (eval . (eglot-ensure))
+   (eval . (add-hook 'before-save-hook #'eglot-format-buffer nil t))))
