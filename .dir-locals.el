1a112c29745fb3041fe5402d0fb0a7d50d7fc267
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-02 16:15:58 +0800

    format

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,4 @@
    (indent-tabs-mode . nil)
    (c-file-style . "bsd")
    (c-basic-offset . 2)
-   (eval . (eglot-ensure))
-   (eval . (add-hook 'before-save-hook #'eglot-format-buffer nil t))))
+   (eval . (eglot-ensure))))
