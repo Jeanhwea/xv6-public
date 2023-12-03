f77e64f6fc2ed50b9b190ce6c855230bcd49de02
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-03 11:43:50 +0800

    update: format dinode comment and ignore cscope.*

--- fs.h
+++ fs.h
@@ -27,12 +27,12 @@ struct superblock {
 
 // On-disk inode structure
 struct dinode {
-  short type;           // File type
-  short major;          // Major device number (T_DEV only)
-  short minor;          // Minor device number (T_DEV only)
-  short nlink;          // Number of links to inode in file system
-  uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  short type;            // File type
+  short major;           // Major device number (T_DEV only)
+  short minor;           // Minor device number (T_DEV only)
+  short nlink;           // Number of links to inode in file system
+  uint size;             // Size of file (bytes)
+  uint addrs[NDIRECT+1]; // Data block addresses
 };
 
 // Inodes per block.
@@ -54,4 +54,3 @@ struct dirent {
   ushort inum;
   char name[DIRSIZ];
 };
-
